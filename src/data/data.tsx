6c01c5edce20fe8995c144e8722becc3d687d4db
rpirayadi <59181565+rpirayadi@users.noreply.github.com>
2023-10-09 22:19:18 +0330

    initial info

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -69,18 +69,17 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `Rouzbeh Pirayadi`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a undergradueate student <strong className="text-stone-100">in Computer Engineering </strong> 
+        at <strong className="text-stone-100">Sharif University of technology</strong>.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        I'm interested in <strong className="text-stone-100"> in memory processing </strong>,
+        <strong className="text-stone-100"> AI accelerators </strong> and other{' '}
+        <strong className="text-stone-100">Computer Architecture </strong> related topics.
       </p>
     </>
   ),
@@ -91,11 +90,11 @@ export const heroData: Hero = {
       primary: true,
       Icon: ArrowDownTrayIcon,
     },
-    {
-      href: `#${SectionId.Contact}`,
-      text: 'Contact',
-      primary: false,
-    },
+    // {
+    //   href: `#${SectionId.Contact}`,
+    //   text: 'Contact',
+    //   primary: false,
+    // },
   ],
 };
 
@@ -108,12 +107,11 @@ export const aboutData: About = {
   to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
   in.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Tehran, Iran', Icon: MapIcon},
+    {label: 'Age', text: '22', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Iranian', Icon: FlagIcon},
+    {label: 'Interests', text: 'In memory processing, AI accelerators, Hardware Architecture', Icon: SparklesIcon},
+    {label: 'Study', text: 'Sharif University of Technology', Icon: AcademicCapIcon},
   ],
 };
 
@@ -269,8 +267,8 @@ export const portfolioItems: PortfolioItem[] = [
 export const education: TimelineItem[] = [
   {
     date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
+    location: 'Sharif University of Technology',
+    title: 'Bachelors of Science in Computer Engineering',
     content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
   },
   {
