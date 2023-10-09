b1e3f3073461fead851ebf7908a49e9d00acc7f3
rpirayadi <59181565+rpirayadi@users.noreply.github.com>
2023-10-09 22:58:58 +0330

    delete contact

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -28,8 +28,6 @@ import profilepic from '../images/rouzbeh2.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
-  ContactSection,
-  ContactType,
   Hero,
   HomepageMeta,
   PortfolioItem,
@@ -53,7 +51,6 @@ export const homePageMeta: HomepageMeta = {
 export const SectionId = {
   Hero: 'hero',
   About: 'about',
-  Contact: 'contact',
   Portfolio: 'portfolio',
   Resume: 'resume',
   Skills: 'skills',
@@ -346,32 +343,32 @@ export const testimonial: TestimonialSection = {
  * Contact section
  */
 
-export const contact: ContactSection = {
-  headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
-  items: [
-    {
-      type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
-    },
-    {
-      type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
-    },
-    {
-      type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
-    },
-  ],
-};
+// export const contact: ContactSection = {
+//   headerText: 'Get in touch.',
+//   description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+//   items: [
+//     {
+//       type: ContactType.Email,
+//       text: 'reachout@timbaker.me',
+//       href: 'mailto:reachout@timbaker.me',
+//     },
+//     {
+//       type: ContactType.Location,
+//       text: 'Victoria BC, Canada',
+//       href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+//     },
+//     {
+//       type: ContactType.Instagram,
+//       text: '@tbakerx',
+//       href: 'https://www.instagram.com/tbakerx/',
+//     },
+//     {
+//       type: ContactType.Github,
+//       text: 'tbakerx',
+//       href: 'https://github.com/tbakerx',
+//     },
+//   ],
+// };
 
 /**
  * Social items
